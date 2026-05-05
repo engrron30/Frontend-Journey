aa876c1faa79a7a1a425bcc08f4dae1b7e534de2
engrron30 <ronaldtorresrosete30@gmail.com>
2026-05-06 00:28:31 +0800

    Create Appointment interface and display via one-way data binding

--- 02_Angular/appointment-app/src/app/app.component.ts
+++ 02_Angular/appointment-app/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Appointment } from './models/appointment';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +6,5 @@ import { Appointment } from './models/appointment';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  appointment: Appointment = {
-    id: 2,
-    title: 'Code more!',
-    date: new Date('2024-07-01T10:00:00')
-  };
+  title = 'appointment-app';
 }
--- 02_Angular/appointment-app/src/app/appointment-list/appointment-list.component.ts
+++ 02_Angular/appointment-app/src/app/appointment-list/appointment-list.component.ts
@@ -7,9 +7,32 @@ import { Appointment } from '../models/appointment';
   styleUrls: ['./appointment-list.component.css']
 })
 export class AppointmentListComponent {
-  appointment: Appointment = {
+  /*appointment: Appointment = {
     id: 69,
     title: 'Code More Angular',
     date: new Date('2026-05-05')
+  }*/
+
+  newAppointmentTitle: string = "";
+  newAppointmentDate: Date = new Date();
+  appointment: Appointment[] = [];
+
+  addAppointment() {
+    if (this.newAppointmentTitle.trim().length && this.newAppointmentDate) {
+      let newAppoint: Appointment = {
+        id: Date.now(),
+        title: this.newAppointmentTitle,
+        date: this.newAppointmentDate
+      }
+
+      console.log('Title:', this.newAppointmentTitle);
+      console.log('Date:', this.newAppointmentDate);
+      this.appointment.push(newAppoint);
+    } else {
+      alert(this.newAppointmentTitle + " " + this.newAppointmentDate);
+    }
+
+    this.newAppointmentTitle = "";
+    this.newAppointmentDate = new Date();
   }
 }
